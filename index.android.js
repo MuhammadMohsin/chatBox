3d5e17273b25a205ed968efb97797cf2bc7d6e18
Mohsin <mmohsinkhalid@outlook.com>
2017-03-31 18:35:06 +0500

    Implemented basic routing

--- index.android.js
+++ index.android.js
@@ -12,35 +12,121 @@ import {
   View
 } from 'react-native';
 
-export default class ChatBox extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Hello React Native
-        </Text>
+import {
+    Header,
+    Link,
+    nativeHistory,
+    Route,
+    Router,
+    StackRoute,
+    withRouter,
+    NativeRouter,
+} from 'react-router-native';
+
+const Home = () => (
+    <Text style={styles.header}>
+      Home
+    </Text>
+);
+
+const About = () => (
+    <Text style={styles.header}>
+      About
+    </Text>
+);
+
+const Topic = ({ match }) => (
+    <Text style={styles.topic}>
+      {match.params.topicId}
+    </Text>
+);
+
+const Topics = ({ match }) => (
+    <View>
+      <Text style={styles.header}>Topics</Text>
+      <View>
+        <Link
+            to={`${match.url}/rendering`}
+            style={styles.subNavItem}
+            underlayColor='#f0f4f7'>
+          <Text>Rendering with React</Text>
+        </Link>
+        <Link
+            to={`${match.url}/components`}
+            style={styles.subNavItem}
+            underlayColor='#f0f4f7'>
+          <Text>Components</Text>
+        </Link>
+        <Link
+            to={`${match.url}/props-v-state`}
+            style={styles.subNavItem}
+            underlayColor='#f0f4f7'>
+          <Text>Props v. State</Text>
+        </Link>
       </View>
-    );
-  }
-}
+
+      <Route path={`${match.url}/:topicId`} component={Topic}/>
+      <Route exact path={match.url} render={() => (
+          <Text style={styles.topic}>Please select a topic.</Text>
+      )} />
+    </View>
+);
+
+export default ChatBox = () => (
+    <NativeRouter>
+      <View style={styles.container}>
+        <View style={styles.nav}>
+          <Link
+              to="/"
+              underlayColor='#f0f4f7'
+              style={styles.navItem}>
+            <Text>Home</Text>
+          </Link>
+          <Link
+              to="/about"
+              underlayColor='#f0f4f7'
+              style={styles.navItem}>
+            <Text>About</Text>
+          </Link>
+          <Link
+              to="/topics"
+              underlayColor='#f0f4f7'
+              style={styles.navItem} >
+            <Text>Topics</Text>
+          </Link>
+        </View>
+
+        <Route exact path="/" component={Home}/>
+        <Route path="/about" component={About}/>
+        <Route path="/topics" component={Topics}/>
+      </View>
+    </NativeRouter>
+)
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    marginTop: 25,
+    padding: 10,
   },
-  welcome: {
+  header: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  nav: {
+    flexDirection: 'row',
+    justifyContent: 'space-around'
   },
+  navItem: {
+    flex: 1,
+    alignItems: 'center',
+    padding: 10,
+  },
+  subNavItem: {
+    padding: 5,
+  },
+  topic: {
+    textAlign: 'center',
+    fontSize: 15,
+  }
 });
 
 AppRegistry.registerComponent('ChatBox', () => ChatBox);
