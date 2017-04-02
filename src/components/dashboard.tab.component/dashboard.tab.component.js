71f1670d23131e1921f8e767b73f0360d025b869
Mohsin <mmohsinkhalid@outlook.com>
2017-04-03 00:25:40 +0500

    Implemented tabs on dashboard

---
+++ src/components/dashboard.tab.component/dashboard.tab.component.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import Tabs from 'react-native-tabs';
+import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+
+import FriendList from './../friends.list.component/friends.list.component.js';
+import MessageDetails from './../chat.details.component/chat.details.component.js';
+
+class TabsComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {page: 'Contacts'};
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Tabs selected={this.state.page} style={styles.tabs}
+                      selectedStyle={{color:'red'}} onSelect={el=>this.setState({page:el.props.name})}>
+                    <Text name="Contacts" selectedIconStyle={{borderTopWidth:2,borderTopColor:'red'}}>Contacts</Text>
+                    <Text name="Chat" selectedIconStyle={{borderTopWidth:2,borderTopColor:'red'}}>Chat</Text>
+                    <Text name="Settings" selectedIconStyle={{borderTopWidth:2,borderTopColor:'red'}}>Settings</Text>
+                </Tabs>
+                <View style={styles.container}>
+                    {(this.state.page == 'Contacts')? <FriendList />:<MessageDetails />}
+                </View>
+
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        zIndex:10,
+        flex: 1
+    },
+    tabs: {
+        zIndex:20,
+        backgroundColor:'white'
+    }
+});
+
+export default TabsComponent;
