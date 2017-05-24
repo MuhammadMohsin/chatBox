d2c4043e0aecc4fbd8138d66796277b6d3dfd33b
Mohsin <mmohsinkhalid@outlook.com>
2017-05-25 00:47:22 +0500

    Refactored friend list component

--- src/components/friends.list.component/friends.list.component.js
+++ src/components/friends.list.component/friends.list.component.js
@@ -7,6 +7,7 @@ import {
     View
 } from 'react-native';
 import { Link } from 'react-router-native';
+import {Container, Content, Header} from 'native-base';
 import firebase, { firebaseDb } from '../../config/firebase';
 
 import Row from './Row'
@@ -21,7 +22,7 @@ class FriendList extends React.Component {
 
         const ds = new ListView.DataSource({
             rowHasChanged: (r1, r2) => r1 !== r2,
-            sectionHeaderHasChanged : (s1, s2) => s1 !== s2,
+            sectionHeaderHasChanged: (s1, s2) => s1 !== s2,
             getSectionData,
             getRowData,
         });
@@ -31,9 +32,8 @@ class FriendList extends React.Component {
             isLoading: false
         };
 
-        console.log("===========Hello World==================");
         let firebaseRef = firebase.database().ref();
-        firebaseRef.child('users').once('value').then((data)=>{
+        firebaseRef.child('users').once('value').then((data)=> {
             console.log(data.val());
             const { dataBlob, sectionIds, rowIds }  = this.formatData(data.val()[0].Contacts);
             this.setState({dataSource: ds.cloneWithRowsAndSections(dataBlob, sectionIds, rowIds)});
@@ -42,68 +42,59 @@ class FriendList extends React.Component {
     }
 
     formatData(data) {
-        // We're sorting by alphabetically so we need the alphabet
         const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
 
-        // Need somewhere to store our data
         const dataBlob = {};
         const sectionIds = [];
         const rowIds = [];
 
-        // Each section is going to represent a letter in the alphabet so we loop over the alphabet
         for (let sectionId = 0; sectionId < alphabet.length; sectionId++) {
-            // Get the character we're currently looking for
             const currentChar = alphabet[sectionId];
 
-            // Get users whose first name starts with the current letter
             const users = data.filter((user) => user.name.first.toUpperCase().indexOf(currentChar) === 0);
 
-            // If there are any users who have a first name starting with the current letter then we'll
-            // add a new section otherwise we just skip over it
             if (users.length > 0) {
-                // Add a section id to our array so the listview knows that we've got a new section
                 sectionIds.push(sectionId);
+                dataBlob[sectionId] = {character: currentChar};
 
-                // Store any data we would want to display in the section header. In our case we want to show
-                // the current character
-                dataBlob[sectionId] = { character: currentChar };
-
-                // Setup a new array that we can store the row ids for this section
                 rowIds.push([]);
 
-                // Loop over the valid users for this section
                 for (let i = 0; i < users.length; i++) {
-                    // Create a unique row id for the data blob that the listview can use for reference
                     const rowId = `${sectionId}:${i}`;
-
-                    // Push the row id to the row ids array. This is what listview will reference to pull
-                    // data from our data blob
+        
                     rowIds[rowIds.length - 1].push(rowId);
 
-                    // Store the data we care about for this row
                     dataBlob[rowId] = users[i];
                 }
             }
         }
 
-        return { dataBlob, sectionIds, rowIds };
+        return {dataBlob, sectionIds, rowIds};
     }
 
     render() {
-        return (
-            <View style={styles.container}>
-                <View style={styles.header}>
-                    <Text style={styles.contactsTitle}>Contacts</Text>
-                </View>
+        const{
+            header,
+            contactsTitle,
+            container
+        } = styles;
 
-                {this.state.isLoading?
-                    <ListView
-                        style={styles.container}
-                        dataSource={this.state.dataSource}
-                        renderRow={(data) => <Row {...data} />}
-                    />:
-                <Text>Loading..</Text>}
-            </View>
+        return (
+            <Container>
+                <Header style={StyleSheet.flatten(header)}>
+                    <Text style={StyleSheet.flatten(contactsTitle)}>Contacts</Text>
+                </Header>
+                <Content>
+
+                    {this.state.isLoading ?
+                        <ListView
+                            style={container}
+                            dataSource={this.state.dataSource}
+                            renderRow={(data) => <Row {...data} />}
+                            /> :
+                        <Text>Loading..</Text>}
+                </Content>
+            </Container>
         );
     }
 }
@@ -111,11 +102,10 @@ class FriendList extends React.Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        zIndex:0
+        zIndex: 0
     },
     header: {
         backgroundColor: 'rgba(41, 128, 185,0.85)',
-        height: 45,
         justifyContent: 'center',
         alignItems: 'center'
     },
