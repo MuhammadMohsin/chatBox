b4cfcc70700907b65024c137fcc1e368193170c6
Mohsin <mmohsinkhalid@outlook.com>
2017-05-31 16:09:37 +0500

    Fixed web and device dependencies

--- src/web/App.jsx
+++ src/web/App.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import * as React from 'react';;
 
 export default class App extends React.Component {
   render() {
     return (
      <div>
         <h1>Hello World</h1>
-      </div>);
+      </div>
+      );
   }
 }
